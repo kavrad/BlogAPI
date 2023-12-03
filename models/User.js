91a07ce392694c2589598487574290b57d0bc1b9
kavrad <kavyaht39@gmail.com>
2023-12-03 10:21:57 +0530

    Define User model to store user details in db

---
+++ models/User.js
@@ -0,0 +1,33 @@
+//import mongoose
+const mongoose=require("mongoose");
+
+//define schema for users model
+const usersSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    confirmPassword:{
+        type:String,
+       required:true,
+       trim:true
+    },
+    accountType:{
+        type:String,
+        enum:["User","Author"],
+        required:true
+    }
+})
+
+//create users model via mongoose
+module.exports=mongoose.model("User",usersSchema);
