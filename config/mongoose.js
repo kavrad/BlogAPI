9c1160f1cdb6b8055ee4553c503a42b36d260f37
kavrad <kavyaht39@gmail.com>
2023-12-03 10:14:48 +0530

    Establishing connection between server and db via mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+exports.dbConnect=async ()=>{
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }) 
+        console.log("Sucessfully connected to db");
+    } catch (error) {
+        console.error("Error in connecting to db")
+        console.error(error);
+        process.exit(1);
+    }
+    
+   
+}
+
