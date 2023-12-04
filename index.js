5496ef60f1eb58479cdfc72e1376aaa99e24bec0
kavrad <kavyaht39@gmail.com>
2023-12-04 09:14:44 +0530

    Some changes to main file

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ require("dotenv").config();
 //function to connect server to db
 const {dbConnect}=require("./config/mongoose");
 
+//auth routes
+const authRoutes=require("./routes/auth");
+
+//import passport middleware
+const passport=require("passport");
+
+//import blogpost routes
+const postRoutes=require("./routes/post")
+
 //initialize port
 const port=process.env.PORT || 5000
 
@@ -14,7 +23,17 @@ const port=process.env.PORT || 5000
 const app=express();
 
 //Connection to db via mongoose
-await dbConnect();
+dbConnect();
+
+//middleware to parse request body
+app.use(express.json());
+
+//initialize passport middleware
+app.use(passport.initialize())
+
+app.use("/api/v1/auth",authRoutes);
+
+app.use("/api/v1/posts",postRoutes);
 
 //listening the server n a particular port
 app.listen(port,(error)=>{
